872b25ed07f31a69bcc5ac238a3b23c86b5868e3
Tx6B7G <strongduck@foxmail.com>
2021-03-15 16:12:57 +0800

    Update n1.sh

--- n1.sh
+++ n1.sh
@@ -32,6 +32,9 @@ svn co https://github.com/gd0772/package/trunk/luci-app-cpufreq ./package/diy/lu
 # echo '添加 AdGuardHome'
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ./package/diy/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome ./package/diy/AdGuardHome
+# echo '添加 dockerman'
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman ./package/diy/luci-app-dockerman
+sed -i "s:/opt/docker:/mnt/mmcblk2p3/docker:g" package/diy/luci-app-dockerman/root/etc/config/dockerman
 # echo '添加 SSR Plus+'
 git clone https://github.com/fw876/helloworld package/diy/ssrplus
 # echo '添加 小猫咪'
